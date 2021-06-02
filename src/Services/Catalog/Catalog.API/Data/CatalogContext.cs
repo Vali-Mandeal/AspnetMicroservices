11b887f288fffaaef2a10273e2297c0efa9501f7
valeriu.mandeal <valeriu.mandeal@gmail.com>
2021-06-02 12:55:42 +0300

    2.20 Added data layer for Products

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; set; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            MongoClient client = new(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            IMongoDatabase database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(
+                configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
+    
